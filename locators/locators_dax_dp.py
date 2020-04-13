596cc078bbe1c1b400ca3d47964433edd2a8157a
mjovbg <mjov.bg@gmail.com>
2020-04-13 15:19:02 +0200

    Added asserts and waits in detail page testing.

--- locators/locators_dax_dp.py
+++ locators/locators_dax_dp.py
@@ -1,18 +1,43 @@
-
 class LocatorsDax:
+    # TABS ELEMENTS:
+    dax_symbol_id = '514562|symbolName'
+    action_menu_id = 'add-to-icon-symbolDetail'
+    constituents_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td.constituents-tab > table > tbody > tr > td:nth-child(1) > a'
+    overview_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(1) > a'
+    chart_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(2) > a'
+    topflop_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(4)'
+    timessales_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(5)'
+    news_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(6)'
+    options_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(7)'
+    derivatives_css = '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(8)'
 
-    dax_symbol_id           =   '514562|symbolName'
-    action_menu_id          =   'add-to-icon-symbolDetail'
-    constituents_css        =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td.constituents-tab > table > tbody > tr > td:nth-child(1) > a'
-    overview_css            =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(1) > a'
-    chart_css               =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(2) > a'
-    topflop_css             =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(4)'
-    timessales_css          =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(5)'
-    news_css                =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(6)'
-    options_css             =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(7)'
-    derivatives_css         =   '#rightDiv > div.tabs > div.tab-area.funds-tab-area.security.tabs-full-width.tabs-less-number > table > tbody > tr > td:nth-child(8)'
+    # ELEMENTS FOR WAITING ON PAGES:
 
-    # ELEMNTS FOR ASSERTING PAGES:
+class LocatorsDaxWaits:
+    overview_chart_1d_id            = '1D'
+    chart_setdefault_id             = 'set-as-default-button'
+    constituents_columnpicker_id    = 'pricePagePerformancePickerLink'
+    topflop_reload_css              = '#rightDiv > div.tabs > div.tab-main.tab-mainForSecurity > div.pricepage-icon-container > div.pricepage-Reload > div > a'
+    timesales_startdate_id          = 'startDate'
+    news_size_id                    = 'newsFontSizeButtonHeadline'
+    options_reset_id                = 'securitiesFuturesAndOptionsReset'
+    derivatives_columnpicker_id     = 'futures_ttr-413719_Link'
+
+    # ELEMENTS FOR ASSERTING PAGES:
+
+class LocatorsDaxAsserts:
+    overview_minichart_id           =   'minichart-img'
+    chart_savetemplate_id           =   'save-default-chart-template-menu'
+    constituents_quoteboard_css     =   '#rightDiv > div.tabs > div.tab-main.tab-mainForSecurity > div.pricepage-icon-container > div.pricepage-icon.pricepage-icon-QuoteBoard > a'
+    topflop_clos_css                =   '#rightDiv > div.tabs > div.tab-main.tab-mainForSecurity > div.light-box.constituents.topflop-container > div:nth-child(2) > a.active.not-draggable'
+    timesales_starthour_id          =   'startHourMinute'
+    news_ecdata_css                 =   '#bonds-search-area-link > a'
+    options_search_id               =   'securitiesFuturesAndOptionsSearch'
+    derivatives_actionmenu_id       =   'add-to-icon-futures-ttr-413719'
+
+
+
+
 
 
 
--- pages/dax_detail_page.py
+++ pages/dax_detail_page.py
@@ -1,7 +1,7 @@
 from base.selenium_driver import SeleniumDriver
 from locators.locators_login_page import LocatorsLP
 from locators.locators_hp import LocatorsHp
-from locators.locators_dax_dp import LocatorsDax
+from locators.locators_dax_dp import LocatorsDax, LocatorsDaxWaits, LocatorsDaxAsserts
 import time
 
 class DaxDetailPage(SeleniumDriver, LocatorsDax):
@@ -11,39 +11,52 @@ class DaxDetailPage(SeleniumDriver, LocatorsDax):
         # super allows: to avoid using the base class name explicitly; Working with Multiple Inheritance
         super().__init__(driver)
         self.driver = driver
+        global ldw
+        ldw = LocatorsDaxWaits
+        global lda
+        lda = LocatorsDaxAsserts
 
     def click_dax(self):
         self.elementClick(LocatorsDax.dax_symbol_id)
-        time.sleep(3)
+        self.waitForElement(ldw.overview_chart_1d_id)
+        self.isElementPresent(lda.overview_minichart_id)
 
     def click_constituents(self):
         self.elementClick(LocatorsDax.constituents_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.constituents_columnpicker_id)
+        self.isElementPresent(lda.constituents_quoteboard_css, locatorType='css')
 
     def click_overview(self):
         self.elementClick(LocatorsDax.overview_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.overview_chart_1d_id)
+        self.isElementPresent(lda.overview_minichart_id)
 
     def click_chart(self):
         self.elementClick(LocatorsDax.chart_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.chart_setdefault_id)
+        self.isElementPresent(lda.chart_savetemplate_id)
 
     def click_topflop(self):
         self.elementClick(LocatorsDax.topflop_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.topflop_reload_css, locatorType='css')
+        self.isElementPresent(lda.topflop_clos_css, locatorType='css')
 
     def click_timessales(self):
         self.elementClick(LocatorsDax.timessales_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.timesales_startdate_id)
+        self.isElementPresent(lda.timesales_starthour_id)
 
     def click_news(self):
         self.elementClick(LocatorsDax.news_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.news_size_id)
+        self.isElementPresent(lda.news_ecdata_css, locatorType='css')
 
     def click_options(self):
         self.elementClick(LocatorsDax.options_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.options_reset_id)
+        self.isElementPresent(lda.options_search_id)
 
     def click_derivatives(self):
         self.elementClick(LocatorsDax.derivatives_css, locatorType='css')
-        time.sleep(3)
+        self.waitForElement(ldw.derivatives_columnpicker_id)
+        self.isElementPresent(lda.derivatives_actionmenu_id)
